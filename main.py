cc29ddca4077914b00728e80715f05cd2e826a78
Yanjian Zhang <yanjian.zhang@outlook.com>
2025-10-04 15:20:03 +0800

    refactor: clean unused files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-os.environ['JAX_PLATFORM_NAME'] = 'cpu'
+
+os.environ["JAX_PLATFORM_NAME"] = "cpu"
 import pickle
 import argparse
 
@@ -47,35 +48,19 @@ def main():
         results = run_natural_niches(**args_dict)
     elif method == "ga":
         if not args.no_matchmaker:
-            print('WARNING: GA can\'t use matchmaker. Disabling it.')
+            print("WARNING: GA can't use matchmaker. Disabling it.")
             args_dict["no_matchmaker"] = True
         if args.no_crossover:
             file_name += "_no_crossover"
         if args.no_splitpoint and not args.no_crossover:
             file_name += "_no_splitpoint"
         results = run_natural_niches(**args_dict, alpha=0.0)
-    elif method == "map_elites":
-        if args.no_crossover or args.no_splitpoint or args.no_matchmaker:
-            raise Exception("Map Elites can't use no_crossover, no_splitpoint or no_matchmaker")
-        results = run_map_elites(
-            args.runs, args.total_forward_passes, args.store_train_results, args.use_pre_trained
-        )
-    elif method == "cma_es":
-        assert not args.use_pre_trained, "CMA-ES does not support pre-trained models"
-        if args.no_crossover or args.no_splitpoint or args.no_matchmaker:
-            raise Exception("CMA-ES can't use no_crossover, no_splitpoint or no_matchmaker")
-        results = run_cma_es(
-            args.runs,
-            args.pop_size,
-            args.total_forward_passes,
-            args.store_train_results,
-        )
     elif method == "brute_force":
         assert args.use_pre_trained, "Brute force requires pre-trained models"
         results = run_brute_force()
     else:
         raise NotImplementedError("Method not implemented")
-    
+
     if args.use_pre_trained:
         file_name += "_pre_trained"
 
